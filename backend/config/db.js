96cb1c776d34da4ff09345d8e61e1f49baf08361
Shrey vats <techtechnicalshrey@gmail.com>
2025-06-08 20:06:39 +0530

    setup database

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true, // Deprecated in Mongoose 6+
+      // useFindAndModify: false // Deprecated in Mongoose 6+
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+export default connectDB;
