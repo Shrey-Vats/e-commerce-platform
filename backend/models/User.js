6ae6375c0182ead272585e8421773eb3f957ce45
Shrey vats <techtechnicalshrey@gmail.com>
2025-06-08 20:07:21 +0530

    setup user schema

---
+++ backend/models/User.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt timestamps
+  }
+);
+
+// Method to compare entered password with hashed password in the database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Pre-save hook to hash the password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next(); // If password is not modified, move to the next middleware
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
