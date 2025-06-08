f1ee1aa9c84199866eda1f7408d1558b62c4297d
Shrey vats <techtechnicalshrey@gmail.com>
2025-06-08 22:27:42 +0530

    setup AppRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,18 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { Outlet } from "react-router-dom"; // For nested layouts
+import { ToastContainer } from "react-toastify"; // For toast notifications
+import "react-toastify/dist/ReactToastify.css"; // Toastify CSS
+
+const App = () => {
+  return (
+    <>
+      <ToastContainer /> {/* Where toast notifications will appear */}
+      <main className="min-h-screen flex flex-col">
+        {/* This Outlet will render components matched by react-router-dom */}
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default App;
